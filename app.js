64374595a802ea3130f7588aae524dd3279c0fd0
Artem Mazin <minima1s@yandex.ru>
2023-05-31 23:52:13 +0300

    feat: подключится к базе данных и добавить ф-цию создания пользователя

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import User from './models/user.js';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.post('/users', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err: `${err.message}` }));
 });
 
 app.listen(PORT, () => {
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+// import { Router } from 'express';
+
+// export default Router().get('/users/:id', (req, res) => {
+//   if (!users[req.params.id]) {
+//     res.send(`Такого пользователя не существует`);
+
+//     // не забудем выйти из функции
+//     return;
+//   }
+
+//   const { name, age } = users[req.params.id];
+
+//   res.send(`Пользователь ${name}, ${age} лет`);
+// });
