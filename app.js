5b6ce6acc2c3fc286121eb647c480da0f707e343
Artem Mazin <minima1s@yandex.ru>
2023-05-31 14:50:24 +0300

    feat: создать схемы и модели

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
