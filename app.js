6c0980c24000bfd02ad6945dd06190790eadfb3f
Artem Mazin <minima1s@yandex.ru>
2023-06-01 16:55:34 +0300

    feat: добавить функцию получения пользователей

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '647884f2468bb0be2b612ae9',
+  };
+
+  next();
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -15,6 +22,18 @@ app.post('/users', (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err: `${err.message}` }));
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+app.get('/users/:userId', (req, res) => {
+  User.findById(req.params._id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 app.listen(PORT, () => {
