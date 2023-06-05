7578631fc8c91ed36ea0b18c977e23729cfa3dcd
Artem Mazin <minima1s@yandex.ru>
2023-06-05 12:11:01 +0300

    feat: установить helmet и limiter для защиты приложения

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import router from './routes/index';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Слишком много запросов с данного IP, повторите попытку позднее',
+});
+
+// express-rate-limit ограничивает количество запросов
+app.use(limiter);
+// helmet помогает защитить приложения Express, устанавливая заголовки ответа HTTP
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
