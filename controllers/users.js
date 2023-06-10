828443d73f37f95fea910e00531cb627a2d076f3
Artem Mazin <minima1s@yandex.ru>
2023-06-10 23:50:20 +0300

    feat: добавить поля email, password и сделать хэширование

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,13 @@
+import bcrypt from 'bcryptjs';
 import User from '../models/user';
 import handleErrors from '../utils/utils';
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  User.create({ name, about, avatar })
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({ name, about, avatar, email, password: hash }))
     .then((user) => res.status(201).send({ data: user }))
     .catch((err) => handleErrors(err, res));
 };
@@ -32,7 +35,7 @@ const updateProfile = (req, res) => {
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением
-    },
+    }
   )
     .then((user) => res.send({ data: user }))
     .catch((err) => handleErrors(err, res));
@@ -48,12 +51,10 @@ const updateAvatar = (req, res) => {
     {
       new: true, // обработчик then получит на вход обновлённую запись
       runValidators: true, // данные будут валидированы перед изменением
-    },
+    }
   )
     .then((user) => res.send({ data: user }))
     .catch((err) => handleErrors(err, res));
 };
 
-export {
-  createUser, getUsers, getUserById, updateProfile, updateAvatar,
-};
+export { createUser, getUsers, getUserById, updateProfile, updateAvatar };
--- models/user.js
+++ models/user.js
@@ -17,6 +17,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
 
 export default mongoose.model('user', userSchema);
