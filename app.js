edde0a707226adaad684fde3742715d4d0788e30
Artem Mazin <minima1s@yandex.ru>
2023-06-15 14:43:41 +0300

    fix: сделать исправления

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(router);
 
 app.use(errors());
 
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   res.status(err.statusCode).send({ message: err.message });
 });
 
